0a839acaf3d24ff3979abfbf474d90ec574efaae
Jacob Wagner <jwagner@prontoforms.com>
2020-01-20 17:49:09 -0500

    here ye go

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { ThemeProvider } from "styled-components";
+import Layout from "./Scene/Layout";
+import "./app.css";
 
-function App() {
+const theme = {
+  LightBackground: "#FFFFFF",
+  DarkBackground: "#E7E7E7",
+  Header: "#0076A8",
+  Sidebar: "#2C3F50",
+  Hover: "#678678",
+  Selected: "#F26522",
+  Dropshadow: "#AAAAAA",
+  Text: "#333333"
+};
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [networks, setNetworks] = useState();
+
+  useEffect(() => {
+    if (loading) {
+      fetch("http://api.citybik.es/v2/networks?fields=location.country,href,id")
+        .then(response => response.json())
+        .then(data => {
+          const networks = data.networks;
+          const networkSortedByCountry = networks.reduce(
+            (sorted, item, i) => {
+              if (item.location) {
+                sorted.country[item.location.country] = {
+                  [i]: item,
+                  ...sorted.country[item.location.country]
+                };
+                return sorted;
+              }
+            },
+            { country: {} }
+          );
+          setNetworks(networkSortedByCountry);
+          setLoading(false);
+        });
+    }
+  }, [loading]);
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Layout appData={networks} />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
